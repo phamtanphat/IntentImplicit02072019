ca67dd7c312dce0784fcfaa8ff53eb496565f2af
phatdroid94 <phatdroid94@gmail.com>
2019-09-10 19:22:54 +0700

    a

--- app/src/main/java/phamtanphat/ptp/khoaphamtraining/intentimplicit02072019/MainActivity.java
+++ app/src/main/java/phamtanphat/ptp/khoaphamtraining/intentimplicit02072019/MainActivity.java
@@ -1,14 +1,48 @@
 package phamtanphat.ptp.khoaphamtraining.intentimplicit02072019;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btnCamera,btnGallery;
+    ImageView img;
+    int Request_Code_Camera = 123;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        img = findViewById(R.id.imageview);
+        btnCamera = findViewById(R.id.buttonCamera);
+        btnGallery = findViewById(R.id.buttonGallery);
+
+        btnCamera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ActivityCompat.requestPermissions(
+                        MainActivity.this,
+                        new String[]{Manifest.permission.CAMERA},
+                        Request_Code_Camera);
+            }
+        });
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == Request_Code_Camera){
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED){
+
+            }
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
     }
 }
---
+++ app/src/main/java/phamtanphat/ptp/khoaphamtraining/intentimplicit02072019/MainViewModel.java
@@ -0,0 +1,11 @@
+package phamtanphat.ptp.khoaphamtraining.intentimplicit02072019;
+
+import androidx.lifecycle.LifecycleObserver;
+
+public class MainViewModel implements LifecycleObserver {
+
+    public MainViewModel() {
+
+    }
+
+}
