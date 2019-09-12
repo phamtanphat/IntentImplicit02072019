efe3d104d0e406cd0abe494109298ac7ee7d4a04
phatdroid94 <phatdroid94@gmail.com>
2019-09-12 18:11:36 +0700

    a

--- app/src/main/java/phamtanphat/ptp/khoaphamtraining/intentimplicit02072019/MainActivity.java
+++ app/src/main/java/phamtanphat/ptp/khoaphamtraining/intentimplicit02072019/MainActivity.java
@@ -1,12 +1,18 @@
 package phamtanphat.ptp.khoaphamtraining.intentimplicit02072019;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
+import androidx.lifecycle.LifecycleOwner;
+import androidx.lifecycle.Observer;
 
 import android.Manifest;
+import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -16,14 +22,26 @@ public class MainActivity extends AppCompatActivity {
     Button btnCamera,btnGallery;
     ImageView img;
     int Request_Code_Camera = 123;
+    MainViewModel mainViewModel;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mainViewModel = new MainViewModel();
+        getLifecycle().addObserver(mainViewModel);
 
         img = findViewById(R.id.imageview);
         btnCamera = findViewById(R.id.buttonCamera);
         btnGallery = findViewById(R.id.buttonGallery);
+
+        mainViewModel.mBitmap.observe(this, new Observer<Bitmap>() {
+            @Override
+            public void onChanged(Bitmap bitmap) {
+                if (bitmap != null){
+                    img.setImageBitmap(bitmap);
+                }
+            }
+        });
 
         btnCamera.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -40,9 +58,19 @@ public class MainActivity extends AppCompatActivity {
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         if (requestCode == Request_Code_Camera){
             if (grantResults[0] == PackageManager.PERMISSION_GRANTED){
-
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(intent,Request_Code_Camera);
             }
         }
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if (requestCode == Request_Code_Camera && resultCode == RESULT_OK && data != null){
+            Bitmap bitmap = (Bitmap) data.getExtras().get("data");
+            mainViewModel.setmBitmap(bitmap);
+        }
+        super.onActivityResult(requestCode, resultCode, data);
     }
 }
--- app/src/main/java/phamtanphat/ptp/khoaphamtraining/intentimplicit02072019/MainViewModel.java
+++ app/src/main/java/phamtanphat/ptp/khoaphamtraining/intentimplicit02072019/MainViewModel.java
@@ -1,11 +1,31 @@
 package phamtanphat.ptp.khoaphamtraining.intentimplicit02072019;
 
+import android.database.Observable;
+import android.graphics.Bitmap;
+
+import androidx.lifecycle.Lifecycle;
 import androidx.lifecycle.LifecycleObserver;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.OnLifecycleEvent;
 
 public class MainViewModel implements LifecycleObserver {
 
     public MainViewModel() {
 
     }
+    MutableLiveData<Bitmap> mBitmap = new MutableLiveData<>();
+    public void setmBitmap(Bitmap bitmap){
+        mBitmap.setValue(bitmap);
+    }
 
+//    @OnLifecycleEvent(Lifecycle.Event.ON_CREATE)
+//    public void createMutableliveData(){
+//        mBitmap = new MutableLiveData<>();
+//    }
+//
+//    @OnLifecycleEvent(Lifecycle.Event.ON_STOP)
+//    public void destroyMutableliveData(){
+//        mBitmap = null;
+//    }
 }
